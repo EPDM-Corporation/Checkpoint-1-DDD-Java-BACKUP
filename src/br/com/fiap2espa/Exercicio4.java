c8477fc1c1c2a7056a667d91b3a6b74c64957d97
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-09-01 10:28:08 -0300

    feat: Exercício 4

--- src/br/com/fiap2espa/Exercicio4.java
+++ src/br/com/fiap2espa/Exercicio4.java
@@ -1,4 +1,22 @@
 package br.com.fiap2espa;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira o número para fatoriar: ");
+        int user_fatorial = scanner.nextInt();
+
+        int resultado_fatorial = fatorial(user_fatorial);
+        System.out.println("A fatorial de " + user_fatorial + " é " + resultado_fatorial);
+    }
+     static int fatorial(int input_fatorial){
+        int result = 1;
+        while (input_fatorial > 0){
+            result *= input_fatorial;
+            input_fatorial -= 1;
+        }
+        return result;
+    }
 }
