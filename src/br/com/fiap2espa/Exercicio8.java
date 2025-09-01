dd0ba2179507b8c1827ffd6b3a876ac3f06b5b23
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-09-01 10:54:18 -0300

    feat: Exercício 8

---
+++ src/br/com/fiap2espa/Exercicio8.java
@@ -0,0 +1,28 @@
+package br.com.fiap2espa;
+
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira a sua altura: ");
+        double user_altura = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Insira o seu peso: ");
+        double user_peso = scanner.nextDouble();
+        scanner.nextLine();
+        double IMC = user_peso/user_altura;
+
+        if (IMC < 18.5){
+            System.out.println("Seu IMC é " + IMC + " e a classificação é: abaixo do peso");
+        } else if (IMC >= 18.5 && IMC <=24.9){
+            System.out.println("Seu IMC é " + IMC + " e a classificação é: peso normal");
+        } else if (IMC >= 25 && IMC <= 29.9){
+            System.out.println("Seu IMC é " + IMC + " e a classificação é: Sobrepeso");
+        }else{
+            System.out.println("Seu IMC é " + IMC + " e a classificação é: Obesidade");
+        }
+        scanner.close();
+    }
+}
