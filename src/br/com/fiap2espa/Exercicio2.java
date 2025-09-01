cfd6fa882e694556c68a365b01dc296e4efd0a3a
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-09-01 10:02:13 -0300

    feat: Exercício 2

---
+++ src/br/com/fiap2espa/Exercicio2.java
@@ -0,0 +1,18 @@
+package br.com.fiap2espa;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira a sua idade para saber se pode votar: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        if (idade >= 16){
+            System.out.println("Você pode votar!");
+        } else{
+            System.out.println("Você não pode votar!");
+        }
+    }
+}
