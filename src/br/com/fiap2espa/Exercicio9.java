67d3590e2dfb1b19571940829b95b8dde0124951
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-09-06 09:30:57 -0300

    Update Exercicio9.java

--- src/br/com/fiap2espa/Exercicio9.java
+++ src/br/com/fiap2espa/Exercicio9.java
@@ -6,13 +6,13 @@ public class Exercicio9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Insira o primeiro número: ");
-        double user_a = scanner.nextInt();
+        double user_a = scanner.nextDouble();
         scanner.nextLine();
         System.out.println("Insira o segundo número: ");
-        double user_b = scanner.nextInt();
+        double user_b = scanner.nextDouble();
         scanner.nextLine();
         System.out.println("Insira o terceiro número: ");
-        double user_c = scanner.nextInt();
+        double user_c = scanner.nextDouble();
         scanner.nextLine();
 
         double result = media(user_a,user_b,user_c);
